def22aa2f654fc90872051a3eb244bc9ae7c2ac4
nigmata <fchiariglione@gmail.com>
2025-08-23 21:20:43 -0300

    arregla sintaxis

--- app.py
+++ app.py
@@ -10,11 +10,5 @@ def index():
 def tipo(name):
     return render_template("name.html", content=name)
 
-@app.route("/admin")
-
-# Ahi en url_for hay que usar una funcion
-def admin():
-    return redirect(url_for("hser", name="Admin!"))
-
 if __name__ == "__maiin__":
     app.run(debug=True)
