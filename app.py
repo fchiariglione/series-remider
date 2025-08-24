c0afb0d73a535f91c58f53fe954b99c929695032
Francisco Chairiglione <fchiariglione@gmail.com>
2025-08-24 19:20:52 -0300

    agrega metodos y formularios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,9 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/<name>")
-def tipo(name):
-    return render_template("name.html", content=name)
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+
+@app.route('/<usr>')
+def user(usr):
+    return render_template("user.html", content=usr)
 
 if __name__ == "__main__":
     app.run(debug=True)
